5fc34222a62f72e0fd0528c772ff85b768c5ade2
sambatranaharizaka <126649552+sambatranaharizaka@users.noreply.github.com>
2024-04-16 11:38:58 +0300

    Update index_lang.php

--- index_lang.php
+++ index_lang.php
@@ -1,20 +1,7 @@
 <?php
-// Obtient le chemin du répertoire courant
-$directory = __DIR__;
-
-// Ouvre le répertoire
-if ($handle = opendir($directory)) {
-    // Affiche un en-tête pour la liste des fichiers
-    echo "Liste des fichiers dans le répertoire :\n";
-    // Parcourt chaque fichier dans le répertoire
-    while (false !== ($file = readdir($handle))) {
-        // Exclut les répertoires spéciaux '.' et '..'
-        if ($file != "." && $file != "..") {
-            // Affiche le nom du fichier
-            echo "$file\n";
-        }
-    }
-    // Ferme le gestionnaire du répertoire
-    closedir($handle);
+$chemin = './';
+$elements = glob($chemin . '*', GLOB_MARK);
+foreach($elements as $element){
+    echo basename($element),"<br>";
 }
 ?>
